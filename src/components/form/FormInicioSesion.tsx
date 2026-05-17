964d552ac671d2d9ab1e742479300f5f5b6344fb
Rebeca Poma <mrebecapoma.6@gmail.com>
2026-05-17 21:58:27 +0200

    fix

--- src/components/form/FormInicioSesion.tsx
+++ src/components/form/FormInicioSesion.tsx
@@ -8,7 +8,6 @@ import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import type { AppRole } from "@/interfaces/Profile";
-import { supabase } from "@/database/supabase/Client";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
 import { KeyRound, Mail } from "lucide-react";
 
